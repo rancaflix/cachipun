63ec099f0d8f69382bb06dd4cf4eb4ae6784e552
rancaflix <cristobalpozom@gmail.com>
2023-02-09 02:28:47 -0300

    UI básica implementada

--- javascript.js
+++ javascript.js
@@ -4,14 +4,19 @@ function getComputerChoice() {
     return moves[randomIndex];
 }
 
-let playerSelection = prompt("Elige entre piedra, papel o tijera").toLowerCase();
+// let playerSelection = prompt("Elige entre piedra, papel o tijera").toLowerCase();
 let computerSelection = getComputerChoice();
 
 let playerScore = 0;
 let computerScore = 0;
 
+const piedra = document.getElementById("piedra");
+const papel = document.getElementById("papel");
+const tijera = document.getElementById("tijera");
+const display = document.getElementById("result");
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+    if (playerSelection == computerSelection) {
         return "¡Es un empate!";
     }
     else if (playerSelection === 'piedra') {
@@ -58,19 +63,68 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-  while (playerScore < 5 && computerScore < 5) {
-    playerSelection = prompt("Elige entre piedra, papel o tijera").toLowerCase();
-    computerSelection = getComputerChoice();
-    playRound(playerSelection,computerSelection)
-    alert("Jugador " + playerScore + " - " + computerScore + " Computador") 
-  }
-  if (playerScore == 5) {
-    alert('¡Ganaste!')
-  }
-  else {
-    alert('¡Perdiste!')
-  }
+const result = document.createElement('p');
+
+
+const clickPiedra = function() {
+  playRound('piedra', getComputerChoice());
+  result.textContent = 'Jugador ' + playerScore + ' - ' + computerScore + ' Computador';
+  if (playerScore >= 5 || computerScore >= 5) {
+    stopGame();
+  };
+};
+
+const clickPapel = function() {
+  playRound('papel', getComputerChoice());
+  result.textContent = 'Jugador ' + playerScore + ' - ' + computerScore + ' Computador';
+  if (playerScore >= 5 || computerScore >= 5) {
+    stopGame();
+  };
 }
 
-console.log(game());
+const clickTijera = function() {
+  playRound('tijera', getComputerChoice());
+  result.textContent = 'Jugador ' + playerScore + ' - ' + computerScore + ' Computador';
+  if (playerScore >= 5 || computerScore >= 5) {
+    stopGame();
+  };
+}
+
+piedra.addEventListener("click", clickPiedra);
+papel.addEventListener("click", clickPapel);
+tijera.addEventListener("click", clickTijera);
+
+display.appendChild(result);
+
+function stopGame() {
+  piedra.removeEventListener("click", clickPiedra);
+  papel.removeEventListener("click", clickPapel);
+  tijera.removeEventListener("click", clickTijera);
+  if (playerScore == 5) {
+    const won = document.createElement('p');
+    won.textContent = 'Ganaste!';
+    display.appendChild(won);
+  }
+  else if (computerScore == 5) {
+    const lost = document.createElement('p');
+    lost.textContent = 'Perdiste!';
+    display.appendChild(lost);
+  }
+};
+
+// function game() {
+//   while (playerScore < 5 && computerScore < 5) {
+//     playerSelection = prompt("Elige entre piedra, papel o tijera").toLowerCase();
+//     computerSelection = getComputerChoice();
+//     playRound(playerSelection,computerSelection)
+//     alert("Jugador " + playerScore + " - " + computerScore + " Computador") 
+//   }
+//   if (playerScore == 5) {
+//     alert('¡Ganaste!')
+//   }
+//   else {
+//     alert('¡Perdiste!')
+//   }
+// }
+
+// console.log(game());
